326ef8a173fe4a39438555ddbf003005c4af2d11
giuliano <giulianoc@catrasoftware.it>
2018-06-05 18:23:57 +0200

    profiles...

--- etc/ffmpegHelper.sh
+++ etc/ffmpegHelper.sh
@@ -2,10 +2,11 @@
 
 CatraMMS_PATH=/home/mms/catramms
 
-export LD_LIBRARY_PATH=$CatraMMS_PATH/ffmpeg-3.4.2/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CatraMMS_PATH/ffmpeg-3.4.2/lib
+export PATH=$PATH:$CatraMMS_PATH/ffmpeg-3.4.2/bin
 
 
-/home/mms/catramms/ffmpeg-3.4.2/bin/ffmpeg -formats
+#/home/mms/catramms/ffmpeg-3.4.2/bin/ffmpeg -formats
 
 #Display options specific to, and information about, a particular muxer:
 #/home/mms/catramms/ffmpeg-3.4.2/bin/ffmpeg -h muxer=matroska
@@ -13,14 +14,14 @@ export LD_LIBRARY_PATH=$CatraMMS_PATH/ffmpeg-3.4.2/lib
 #Display options specific to, and information about, a particular demuxer:
 #/home/mms/catramms/ffmpeg-3.4.2/bin/ffmpeg -h demuxer=gif
 
-/home/mms/catramms/ffmpeg-3.4.2/bin/ffmpeg -codecs
+#/home/mms/catramms/ffmpeg-3.4.2/bin/ffmpeg -codecs
 
-/home/mms/catramms/ffmpeg-3.4.2/bin/ffmpeg -encoders
+#/home/mms/catramms/ffmpeg-3.4.2/bin/ffmpeg -encoders
 
 #Display options specific to, and information about, a particular encoder:
 #/home/mms/catramms/ffmpeg-3.4.2/bin/ffmpeg -h encoder=mpeg4
 
-/home/mms/catramms/ffmpeg-3.4.2/bin/ffmpeg -decoders
+#/home/mms/catramms/ffmpeg-3.4.2/bin/ffmpeg -decoders
 
 #Display options specific to, and information about, a particular decoder:
 #/home/mms/catramms/ffmpeg-3.4.2/bin/ffmpeg -h decoder=aac
