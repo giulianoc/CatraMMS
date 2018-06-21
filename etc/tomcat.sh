48152d0a0195140d58ad71c2fb6e2fd29031f83b
giulianoc@catrasoftware.it <giulianoc@catrasoftware.it>
2018-06-22 00:07:41 +0200

    added GUI

---
+++ etc/tomcat.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]
+then
+	echo "Usage $0 start | stop | status"
+
+	exit
+fi
+
+command=$1
+
+if [ "$command" != "start" -a "$command" != "stop" -a "$command" != "status" ]
+then
+	echo "Usage $0 start | stop  | status[nodaemon]"
+
+	exit
+fi
+
+if [ "$command" == "start" ]
+then
+	sudo systemctl start tomcat
+elif [ "$command" == "status" ]
+then
+	sudo systemctl status tomcat
+elif [ "$command" == "stop" ]
+then
+	sudo systemctl stop tomcat
+fi
+
