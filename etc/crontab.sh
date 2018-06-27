ca9cdc15b70a904441e5c6df29f04bb205a519b6
giuliano <giulianoc@catrasoftware.it>
2018-06-27 16:17:22 +0200

    Crontab script updated

--- etc/crontab.sh
+++ etc/crontab.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+#update certificate
 sudo /usr/bin/certbot --quiet renew --pre-hook "/home/mms/nginx.sh stop" --post-hook "/home/mms/nginx.sh start"
+
+#Retention (3 days)
+find /usr/local/CatraMMS/logs -mtime +3 -type f -delete
+find /usr/local/CatraMMS/storage/MMSGUI/temporaryPushUploads -mtime +3 -type f -delete
+find /usr/local/CatraMMS/storage/IngestionRepository -mtime +3 -type f -delete
+find /usr/local/CatraMMS/storage/MMSWorkingAreaRepository -mtime +3 -type f -delete
 
