65b9c30d04825d41778288c9f84328eaa2683fd7
giuliano <giulianoc@catrasoftware.it>
2019-01-11 19:22:07 +0100

    new workspace editor

--- catramms-gui/catramms/catramms/src/main/java/com/catramms/backing/newWorkspace2/NewWorkflow2.java
+++ catramms-gui/catramms/catramms/src/main/java/com/catramms/backing/newWorkspace2/NewWorkflow2.java
@@ -9,15 +9,23 @@ import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.primefaces.PrimeFaces;
 import org.primefaces.event.FileUploadEvent;
 import org.primefaces.event.NodeSelectEvent;
+import org.primefaces.event.diagram.ConnectEvent;
+import org.primefaces.event.diagram.ConnectionChangeEvent;
+import org.primefaces.event.diagram.DisconnectEvent;
 import org.primefaces.model.DefaultTreeNode;
 import org.primefaces.model.TreeNode;
 import org.primefaces.model.diagram.Connection;
 import org.primefaces.model.diagram.DefaultDiagramModel;
 import org.primefaces.model.diagram.Element;
+import org.primefaces.model.diagram.connector.StraightConnector;
 import org.primefaces.model.diagram.endpoint.DotEndPoint;
+import org.primefaces.model.diagram.endpoint.EndPoint;
 import org.primefaces.model.diagram.endpoint.EndPointAnchor;
+import org.primefaces.model.diagram.endpoint.RectangleEndPoint;
+import org.primefaces.model.diagram.overlay.ArrowOverlay;
 
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
@@ -47,7 +55,10 @@ public class NewWorkflow2 extends Workspace implements Serializable {
 
     private String workflowLabel;
 
+    private String addContentIcon;
+
     private DefaultDiagramModel model;
+    private boolean suspendEvent;
 
     @PostConstruct
     public void init()
@@ -66,27 +77,124 @@ public class NewWorkflow2 extends Workspace implements Serializable {
         }
 
         {
-            model = new DefaultDiagramModel();
-            model.setMaxConnections(-1);
-            model.setConnectionsDetachable(false);
-
-            Element elementA = new Element("A", "20em", "6em");
-            elementA.addEndPoint(new DotEndPoint(EndPointAnchor.BOTTOM));
-
-            Element elementB = new Element("B", "10em", "18em");
-            elementB.addEndPoint(new DotEndPoint(EndPointAnchor.TOP));
-
-            Element elementC = new Element("C", "40em", "18em");
-            elementC.addEndPoint(new DotEndPoint(EndPointAnchor.TOP));
-
-            model.addElement(elementA);
-            model.addElement(elementB);
-            model.addElement(elementC);
-
-            model.connect(new Connection(elementA.getEndPoints().get(0), elementB.getEndPoints().get(0)));
-            model.connect(new Connection(elementA.getEndPoints().get(0), elementC.getEndPoints().get(0)));
+            addContentIcon = "ui-icon-close";
         }
 
+        {
+            model = new DefaultDiagramModel();
+            model.setMaxConnections(-1);
+
+            model.getDefaultConnectionOverlays().add(new ArrowOverlay(20, 20, 1, 1));
+            StraightConnector connector = new StraightConnector();
+            connector.setPaintStyle("{strokeStyle:'#98AFC7', lineWidth:3}");
+            connector.setHoverPaintStyle("{strokeStyle:'#5C738B'}");
+            model.setDefaultConnector(connector);
+
+            Element computerA = new Element(new WorkspaceElement("Computer A", "computer-icon.png"), "10em", "6em");
+            EndPoint endPointCA = createRectangleEndPoint(EndPointAnchor.BOTTOM);
+            endPointCA.setSource(true);
+            computerA.addEndPoint(endPointCA);
+
+            Element computerB = new Element(new WorkspaceElement("Computer B", "computer-icon.png"), "25em", "6em");
+            EndPoint endPointCB = createRectangleEndPoint(EndPointAnchor.BOTTOM);
+            endPointCB.setSource(true);
+            computerB.addEndPoint(endPointCB);
+
+            Element computerC = new Element(new WorkspaceElement("Computer C", "computer-icon.png"), "40em", "6em");
+            EndPoint endPointCC = createRectangleEndPoint(EndPointAnchor.BOTTOM);
+            endPointCC.setSource(true);
+            computerC.addEndPoint(endPointCC);
+
+            Element serverA = new Element(new WorkspaceElement("Server A", "server-icon.png"), "15em", "24em");
+            EndPoint endPointSA = createDotEndPoint(EndPointAnchor.AUTO_DEFAULT);
+            serverA.setDraggable(false);
+            endPointSA.setTarget(true);
+            serverA.addEndPoint(endPointSA);
+
+            Element serverB = new Element(new WorkspaceElement("Server B", "server-icon.png"), "35em", "24em");
+            EndPoint endPointSB = createDotEndPoint(EndPointAnchor.AUTO_DEFAULT);
+            serverB.setDraggable(false);
+            endPointSB.setTarget(true);
+            serverB.addEndPoint(endPointSB);
+
+            model.addElement(computerA);
+            model.addElement(computerB);
+            model.addElement(computerC);
+            model.addElement(serverA);
+            model.addElement(serverB);
+        }
+    }
+
+    public void onConnect(ConnectEvent event)
+    {
+        if(!suspendEvent) {
+            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Connected",
+                    "From " + event.getSourceElement().getData() + " To " + event.getTargetElement().getData());
+
+            FacesContext.getCurrentInstance().addMessage(null, msg);
+
+            PrimeFaces.current().ajax().update("form:msgs");
+        }
+        else {
+            suspendEvent = false;
+        }
+    }
+
+    public void onDisconnect(DisconnectEvent event)
+    {
+        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Disconnected",
+                "From " + event.getSourceElement().getData() + " To " + event.getTargetElement().getData());
+
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+
+        PrimeFaces.current().ajax().update("form:msgs");
+    }
+
+    public void onConnectionChange(ConnectionChangeEvent event)
+    {
+        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Connection Changed",
+                "Original Source:" + event.getOriginalSourceElement().getData() +
+                        ", New Source: " + event.getNewSourceElement().getData() +
+                        ", Original Target: " + event.getOriginalTargetElement().getData() +
+                        ", New Target: " + event.getNewTargetElement().getData());
+
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+
+        PrimeFaces.current().ajax().update("form:msgs");
+        suspendEvent = true;
+    }
+
+    private EndPoint createDotEndPoint(EndPointAnchor anchor)
+    {
+        DotEndPoint endPoint = new DotEndPoint(anchor);
+        endPoint.setScope("network");
+        endPoint.setTarget(true);
+        endPoint.setStyle("{fillStyle:'#98AFC7'}");
+        endPoint.setHoverStyle("{fillStyle:'#5C738B'}");
+
+        return endPoint;
+    }
+
+    private EndPoint createRectangleEndPoint(EndPointAnchor anchor)
+    {
+        RectangleEndPoint endPoint = new RectangleEndPoint(anchor);
+        endPoint.setScope("network");
+        endPoint.setSource(true);
+        endPoint.setStyle("{fillStyle:'#98AFC7'}");
+        endPoint.setHoverStyle("{fillStyle:'#5C738B'}");
+
+        return endPoint;
+    }
+
+    public void addTask(String taskType)
+    {
+        mLogger.info("addTask: " + taskType);
+
+        Element serverA = new Element(new WorkspaceElement(taskType, "server-icon.png"), "10em", "6em");
+        EndPoint endPointCC = createRectangleEndPoint(EndPointAnchor.BOTTOM);
+        endPointCC.setSource(true);
+        serverA.addEndPoint(endPointCC);
+        model.addElement(serverA);
     }
 
     private void loadConfigurationParameters()
@@ -157,16 +265,6 @@ public class NewWorkflow2 extends Workspace implements Serializable {
         return properties;
     }
 
-    public void onElementClick() {
-        String id = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("elementId");
-        mLogger.info("user clicked on " + id);
-    }
-
-    public void onElementRightClick() {
-        String id = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("elementId");
-        mLogger.info("user clicked (right) on " + id);
-    }
-
     public String getWorkflowLabel() {
         return workflowLabel;
     }
@@ -181,5 +279,13 @@ public class NewWorkflow2 extends Workspace implements Serializable {
 
     public void setModel(DefaultDiagramModel model) {
         this.model = model;
+    }
+
+    public String getAddContentIcon() {
+        return addContentIcon;
+    }
+
+    public void setAddContentIcon(String addContentIcon) {
+        this.addContentIcon = addContentIcon;
     }
 }
