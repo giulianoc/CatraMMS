be4d42b410326664aec0eea72f5599747a95fb37
giulianoc@catrasoftware.it <giulianoc@catrasoftware.it>
2019-02-10 20:44:33 +0100

    removed catramms-gui

--- catramms-gui/catramms/buildAndRestart.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/usr/local/apache-tomcat/bin/shutdown.sh && ./build.sh && rm -rf /usr/local/apache-tomcat/webapps/catramms* && cp ./catramms/target/catramms.war /usr/local/apache-tomcat/webapps && /usr/local/apache-tomcat/bin/startup.sh
