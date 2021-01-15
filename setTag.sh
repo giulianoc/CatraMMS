226fbb63867c0d8ac937b4e8e50a2af8a124fb95
giuliano <giuliano.catrambone@rsi.ch>
2021-01-15 09:37:28 +0100

    LiveRecorder: added monitorSegmentDuration. LiveDelivery: extended for Live-Recorder with monitorHLS

--- setTag.sh
+++ setTag.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]
+if [ $# -ne 2 ]
 then
-    echo "usage $0 <version tag, i.e.: v1.0.0>
+    echo "usage $0 <version tag, i.e.: v1.0.0> <tag message>"
 
     exit
 fi
 
 tagName=$1
+tagMessage=$2
 
-echo "git tag -a $tagName -m \"Version $tagName\""
-#git tag -a $tagName -m "Version $tagName"
+echo "git tag -a $tagName -m \"Version $tagName: $tagMessage\""
+git tag -a $tagName -m "Version $tagName: $tagMessage"
 
-echo "echo \"$tagName\" > version.txt"
-#echo "$tagName" > version.txt
+echo "echo \"$tagName, $tagMessage\" > version.txt"
+echo "$tagName, $tagMessage" > version.txt
 
