25b4890dbaf0f3369989e881f1c3a44189631f6b
giuliano <giuliano.catrambone@rsi.ch>
2021-02-22 11:57:36 +0100

    Logs: added rotating logs

--- scripts/printLogFileName.sh
+++ scripts/printLogFileName.sh
@@ -19,13 +19,13 @@ fi
 if [ "$component" == "engine" ]
 then
         #lastLogFile=$(find ~/logs/mmsEngineService/ -type f | sort -r | head -n 1)
-        lastLogFile=$(find ~/logs/mmsEngineService/ -type f | sort -r | awk -v logFileNumber=$logFileNumber '{ if (NR == logFileNumber) printf("%s\n", $0) }' )
+        lastLogFile=$(find ~/logs/mmsEngineService/ -type f -exec stat -c '%X %n' {} \; | sort -nr | awk -v logFileNumber=$logFileNumber 'BEGIN{FS=" "} { if (NR == logFileNumber) printf("%s\n", $2) }' )
 elif [ "$component" == "encoder" ]
 then
-        lastLogFile=$(find ~/logs/mmsEncoder/ -type f | sort -r | awk -v logFileNumber=$logFileNumber '{ if (NR == logFileNumber) printf("%s\n", $0) }' )
+        lastLogFile=$(find ~/logs/mmsEncoder/ -type f -exec stat -c '%X %n' {} \; | sort -nr | awk -v logFileNumber=$logFileNumber 'BEGIN{FS=" "} { if (NR == logFileNumber) printf("%s\n", $2) }' )
 elif [ "$component" == "api" ]
 then
-        lastLogFile=$(find ~/logs/mmsAPI/ -type f | sort -r | awk -v logFileNumber=$logFileNumber '{ if (NR == logFileNumber) printf("%s\n", $0) }' )
+        lastLogFile=$(find ~/logs/mmsAPI/ -type f -exec stat -c '%X %n' {} \; | sort -nr | awk -v logFileNumber=$logFileNumber 'BEGIN{FS=" "} { if (NR == logFileNumber) printf("%s\n", $2) }' )
 elif [ "$component" == "tomcat" ]
 then
         lastLogFile="/home/mms/logs/tomcat-gui/catalina.out /home/mms/logs/tomcat-gui/catramms.log /home/mms/logs/tomcat-gui/catrammslib.log /home/mms/logs/tomcat-gui/catramms-rsi-web-services.log /home/mms/logs/tomcat-gui/catramms-web-services.log"
