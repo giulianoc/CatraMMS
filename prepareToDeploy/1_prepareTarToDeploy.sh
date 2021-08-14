fc721f78b22af3003dee0627ab6bb40ea68009e8
giuliano <giuliano.catrambone@rsi.ch>
2021-08-14 19:40:30 +0200

    mmsEngine: added shutdown method

--- prepareToDeploy/1_prepareTarToDeploy.sh
+++ prepareToDeploy/1_prepareTarToDeploy.sh
@@ -41,9 +41,9 @@ git commit -am "$tagMessage"
 
 echo ""
 printf "${RED}"
-echo "git push https://f926e5b9eb7147ba06f14d700fb89d90ccfc2989@github.com/giulianoc/CatraMMS.git"
+echo "git push https://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@github.com/giulianoc/CatraMMS.git"
 printf "${NC}"
-git push https://f926e5b9eb7147ba06f14d700fb89d90ccfc2989@github.com/giulianoc/CatraMMS.git
+git push https://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@github.com/giulianoc/CatraMMS.git
 
 echo ""
 printf "${RED}"
