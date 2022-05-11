5a025b512cbdd169fd068a17907ab0bc17cafbba
giulianoc <giulianocatrambone@gmail.com>
2022-05-11 19:39:43 +0200

    added getMediaItemsList script

---
+++ scripts/examples/getMediaItemsList/getMediaItemsList.sh
@@ -0,0 +1,2 @@
+
+curl -k -v -X POST -u 1:HNVOoVhHx0yoWNIxFu-ThBA1vAPEKWsxneGgze6eoddEv9aB1xp9NzsBKDBDMEFN -d '{"otherMediaItemsKey": [], "tagsIn": ["PROMO"], "tagsNotIn": [], "responseFields": {"mediaItemKey": true, "title": true } }' -H "Content-Type: application/json" "https://mms-api.mms-cloud-hub.com/catramms/1.0.1/mediaItem?start=0&rows=200&contentType=image&liveRecordingChunk=false&orderBy=mi.title"
