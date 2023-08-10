27060e54f9dcb0c967a46890f263309394fde8ae
giulianoc <giulianocatrambone@gmail.com>
2023-08-10 12:24:17 +0200

    more logs

--- MMSEngine/src/Validator.cpp
+++ MMSEngine/src/Validator.cpp
@@ -209,11 +209,6 @@ void Validator::validateGroupOfTasksMetadata(int64_t workspaceKey,
 void Validator::validateEvents(int64_t workspaceKey, Json::Value taskOrGroupOfTasksRoot,
 	bool validateDependenciesToo)
 {
-{
-string errorMessage = __FILEREF__ + "validateEvents"
-+ ", taskOrGroupOfTasksRoot: " + JSONUtils::toString(taskOrGroupOfTasksRoot);
-_logger->info(errorMessage);
-}    
     string field = "onSuccess";
     if (JSONUtils::isMetadataPresent(taskOrGroupOfTasksRoot, field))
     {
