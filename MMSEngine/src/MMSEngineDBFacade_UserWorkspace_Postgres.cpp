b14f84eba448527c031c3d574a4e7e9eed998298
giulianoc <giulianocatrambone@gmail.com>
2023-11-25 16:28:50 +0100

    postgres fix

--- MMSEngine/src/MMSEngineDBFacade_UserWorkspace_Postgres.cpp
+++ MMSEngine/src/MMSEngineDBFacade_UserWorkspace_Postgres.cpp
@@ -3046,6 +3046,7 @@ Json::Value MMSEngineDBFacade::getWorkspaceDetailsRoot (
 		field = "maxStorageInMB";
 		workspaceDetailRoot[field] = row["maxStorageInMB"].as<int>();
 
+		SPDLOG_INFO("workspaceDetailRoot: {}", JSONUtils::toString(workspaceDetailRoot));
 		{
 			int64_t workSpaceUsageInBytes;
 
