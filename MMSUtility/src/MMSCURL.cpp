6507fd0bbc7dbb0c61c54765bed7e417db8b653d
giulianoc <giulianocatrambone@gmail.com>
2025-01-01 17:12:06 +0100

    rimosso curlpp per httpget

--- MMSUtility/src/MMSCURL.cpp
+++ MMSUtility/src/MMSCURL.cpp
@@ -766,12 +766,18 @@ size_t curlWriteResponseCallback(char *ptr, size_t size, size_t nmemb, void *f)
 	{
 		SPDLOG_INFO(
 			"curlWriteResponseCallback"
-			", size: {}",
-			size
+			", size: {}"
+			", nmemb: {}",
+			size, nmemb
 		);
 		string *response = (string *)f;
 
 		response->append(ptr, size * nmemb);
+		SPDLOG_INFO(
+			"curlWriteResponseCallback"
+			", response: {}",
+			*response
+		);
 
 		return size * nmemb;
 	}
