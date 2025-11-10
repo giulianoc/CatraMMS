bc9ae049934f8e4510def1df70c6d92980b54818
giulianoc <giulianocatrambone@gmail.com>
2025-11-10 22:06:15 +0100

    update pem path

--- scripts/mmsCloudLoop-crontab.sh
+++ scripts/mmsCloudLoop-crontab.sh
@@ -2,6 +2,16 @@
 
 source ~/mms/conf/mms-env.sh
 
+if [ ! -f "$debugFilename" ]; then
+	echo "" > $debugFilename
+else
+	filesize=$(stat -c %s $debugFilename)
+	if [ $filesize -gt 10000000 ]
+	then
+		echo "" > $debugFilename
+	fi
+fi
+
 pgrep -f "mmsCloudLoop.sh prod" > /dev/null
 if [ $? -ne 0 ]; then
   TELEGRAM_GROUPALARMS_BOT_TOKEN=$MMS_TELEGRAM_GROUPALARMS_BOT_TOKEN_PROD TELEGRAM_GROUPALARMS_ID=$MMS_TELEGRAM_GROUPALARMS_ID_PROD nohup /opt/catrasoftware/CatraMMS/scripts/mmsCloudLoop.sh prod > /dev/null &
